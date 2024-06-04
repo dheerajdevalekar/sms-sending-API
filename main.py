c54d237b716b06629723fe568e86b41612c4f31b
3D-Iam-New-PC <dheeraj.d@smart-iam.com>
2024-06-04 18:59:10 +0530

    Modify main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+from uvicorn import run
+from loguru import logger
+from fastapi import FastAPI
+from src.routes.all_routes import router
 
+service_port_num = 18457
+
+app = FastAPI(title="Gateway for Sending SMS")
+app.include_router(router)
+
+
+if __name__ == '__main__':
+    logger.info("Starting SMS Sending Service....!!!!!")
+
+    run("main:app", host="0.0.0.0", port=service_port_num, reload=True)
