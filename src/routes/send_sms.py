6e03c59b0110c2d14afaeaf90cb0e4c0bf857f0e
3D-Iam-New-PC <dheeraj.d@smart-iam.com>
2024-06-05 11:34:13 +0530

    Get a contact numbers in String Format and for API keep timeout of 3secs

--- src/routes/send_sms.py
+++ src/routes/send_sms.py
@@ -1,6 +1,7 @@
+# from time import sleep
 from requests import get
 from loguru import logger
-from datetime import datetime
+# from datetime import datetime
 from pydantic import BaseModel
 from fastapi import APIRouter, HTTPException
 
@@ -10,31 +11,37 @@ router = APIRouter()
 
 
 class SMSBasemodel(BaseModel):
-    contact_num: str
+    contact_num: list
     message: str
 
 
 @router.post(path="/send_sms_1", tags=["Send SMS"])
 def send_sms(sms_data: SMSBasemodel):
+    url_num = 1
     try:
-        url_num = 1
-        res = get(
-            url=sms_url[url_num],
-            params={
-                'username': 'cian',
-                'pass': 'Cian1235',
-                'route': 'trans1',
-                'senderid': 'IAMCMS',
-                'numbers': sms_data.contact_num,
-                'message': sms_data.message
-            }
-        )
-        if res.status_code == 200:
-            logger.success(f'Send SMS to {sms_data.contact_num}: {sms_data.message}')
-            return res.json()
-        else:
-            logger.info(f'Unable to send SMS to {sms_data.contact_num}: {sms_data.message}')
-            raise HTTPException(status_code=res.status_code, detail=res.json())
+        return_data = {}
+        m = sms_data.message
+        for each_num in sms_data.contact_num:
+            res = get(
+                url=sms_url[url_num],
+                params={
+                    'username': 'cian',
+                    'pass': 'Cian1235',
+                    'route': 'trans1',
+                    'senderid': 'IAMCMS',
+                    'numbers': each_num,
+                    'message': sms_data.message
+                },
+                timeout=3
+            )
+            if res.status_code == 200:
+                logger.success(f'Send SMS to {each_num}: {sms_data.message}')
+                return_data.update({each_num: {'status_code': res.status_code, 'json_data': res.json()}})
+            else:
+                logger.info(f'Unable to send SMS to {each_num}: {sms_data.message}')
+                return_data.update({each_num: {'status_code': res.status_code, 'json_data': res.json()}})
+            # sleep(1)
+        return return_data
     except Exception as e:
         logger.error(f'Error to send SMS to {sms_data.contact_num}-{sms_data.message}: {e}')
         raise HTTPException(status_code=500, detail=f'{e}')
